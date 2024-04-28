a43d8026cd93358b11d2917b80541fa488428582
JohnBanares <jmbanares02@gmail.com>
2024-04-28 21:05:17 +0100

    style insert

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import './css/App.css';
 import shoe from './image/shoe.png';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { CiSearch } from "react-icons/ci";
+import { IoIosClose } from "react-icons/io";
+
 
 function App() {
 
+	const [insertCheck, setInsertCheck] = useState('');
 	useEffect(() => {
 		// Fetch restaurant data from MongoDB
 		axios.get(`http://localhost:3003/api/products/`)
@@ -16,7 +19,12 @@ function App() {
 		.catch(error => {
 			console.error('Error fetching reviews:', error);
 		});
-		}, []);
+	}, []);
+	
+	const handleInsertCheck = (condtion)=>{
+		setInsertCheck(condtion);
+
+	} 
 
 	return (
 		<div className="home">
@@ -31,25 +39,28 @@ function App() {
 			</div>	
 
 			<div className="buttons">
-				<button>Insert</button>
+				<button onClick={()=> handleInsertCheck(true)}>Insert</button>
 				<button>Delete</button>
 				<button>Update</button>
 			</div>
 		
-			<div className='insertItem'>
+			{/* show insert container if insert button is clicked */}
+			{insertCheck && <div className='insertItem'>
 					<p>Name:<input/></p>
 					<p>Id:<input/></p>
-					<p>Manufacturer:<input/></p>	
+					<p>Brand:<input/></p>	
 
-					<p>Type:<input/></p>
+					{/* <p>Type:<input/></p> */}
 					<p>Price:<input/></p>	
-					<p>Description:<input/></p>
+					{/* <p>Description:<input/></p> */}
 
 					<div className='image'> 
 						<p>Image:</p>		
 						<img src={shoe}/>	
 					</div>
-			</div>
+					<IoIosClose className='close'onClick={()=>handleInsertCheck(false)}/>
+
+			</div>}
 		</div>
 
 	);	
