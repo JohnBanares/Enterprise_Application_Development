97f6c1f706177e245aae44f2a7db028906291ebf
JohnBanares <jmbanares02@gmail.com>
2024-04-30 12:03:11 +0100

    routes

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,21 @@
+import {Link} from "react-router-dom"
+
+
+
+ 
+
+function About(){
+
+   
+    return(
+        <>
+        <div className="aboutPage">
+            <h1>About Page</h1>
+        </div>
+        </>
+    
+        
+    )
+}
+
+export default About
